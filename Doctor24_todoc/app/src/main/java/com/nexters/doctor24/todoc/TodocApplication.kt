a258288f89c2c8f96c9b1419dfcb0107abded492
z0 <ji9310@naver.com>
2020-01-08 10:43:00 +0900

    네이버지도 검토

---
+++ Doctor24_todoc/app/src/main/java/com/nexters/doctor24/todoc/TodocApplication.kt
@@ -0,0 +1,13 @@
+package com.nexters.doctor24.todoc
+
+import android.app.Application
+import com.naver.maps.map.NaverMapSdk
+
+/**
+ * Created by jiyoung on 07/01/2020
+ */
+class TodocApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
