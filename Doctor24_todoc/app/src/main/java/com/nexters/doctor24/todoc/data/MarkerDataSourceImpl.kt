b8f6a70e66fd093cba1194a8ec2bd8ca24b52815
seunghee63 <0603yang@naver.com>
2020-01-23 12:23:53 +0900

    임시저장

--- Doctor24_todoc/app/src/main/java/com/nexters/doctor24/todoc/data/MarkerDataSourceImpl.kt
+++ Doctor24_todoc/app/src/main/java/com/nexters/doctor24/todoc/data/MarkerDataSourceImpl.kt
@@ -5,7 +5,7 @@ import com.nexters.doctor24.todoc.api.APIMarker
 import com.nexters.doctor24.todoc.data.response.ResMapMarker
 import retrofit2.Retrofit
 
-internal class MarkerDataSourceImpl(val retrofit: Retrofit) : MarkerDataSource{
+internal class MarkerDataSourceImpl(private val retrofit: Retrofit) : MarkerDataSource{
     override fun getMarkers(lat:String, long:String, radius: String): LiveData<List<ResMapMarker>> = retrofit.create(
         APIMarker::class.java).getMarkers(lat, long, radius)
 }
