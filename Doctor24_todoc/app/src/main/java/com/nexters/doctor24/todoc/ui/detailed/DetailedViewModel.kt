9e9b87ccde0824ed541a5a338208e0b8fef234c3
song <0603yang@naver.com>
2020-02-10 19:03:46 +0900

    DetailedViewModel 생성

--- Doctor24_todoc/app/src/main/java/com/nexters/doctor24/todoc/ui/detailed/DetailedViewModel.kt
+++ Doctor24_todoc/app/src/main/java/com/nexters/doctor24/todoc/ui/detailed/DetailedViewModel.kt
@@ -1,7 +1,35 @@
 package com.nexters.doctor24.todoc.ui.detailed
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.nexters.doctor24.todoc.base.BaseViewModel
+import com.nexters.doctor24.todoc.base.DispatcherProvider
+import com.nexters.doctor24.todoc.data.detailed.response.DetailedInfoData
+import com.nexters.doctor24.todoc.repository.DetailedInfoRepository
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class DetailedViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+internal class DetailedViewModel(
+    private val dispatchers: DispatcherProvider,
+    private val repo: DetailedInfoRepository
+) : BaseViewModel() {
+
+    private val _detailedData = MutableLiveData<DetailedInfoData>()
+    val detailedData: LiveData<DetailedInfoData> get() = _detailedData
+
+    fun reqDetailedInfo(type: String, facilityId: String) {
+        uiScope.launch(dispatchers.io()) {
+            try {
+                val result = repo.getDetailedInfo(
+                    type = type,
+                    facilityId = facilityId
+                )
+                withContext(dispatchers.main()) {
+                    _detailedData.value = result.body()
+                }
+            } catch (e: Exception) {
+
+            }
+        }
+    }
 }
