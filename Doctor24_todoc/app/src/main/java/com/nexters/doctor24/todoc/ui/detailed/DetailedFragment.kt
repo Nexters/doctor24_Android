e3175a4aca125ea68cfb2d8ced24e4b25cdfa72b
song <0603yang@naver.com>
2020-02-10 21:50:36 +0900

    Detailed 진료 시간 recyclerView 완료

--- Doctor24_todoc/app/src/main/java/com/nexters/doctor24/todoc/ui/detailed/DetailedFragment.kt
+++ Doctor24_todoc/app/src/main/java/com/nexters/doctor24/todoc/ui/detailed/DetailedFragment.kt
@@ -2,9 +2,12 @@ package com.nexters.doctor24.todoc.ui.detailed
 
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.nexters.doctor24.todoc.R
 import com.nexters.doctor24.todoc.base.BaseFragment
 import com.nexters.doctor24.todoc.databinding.DetailedFragmentBinding
+import com.nexters.doctor24.todoc.ui.detailed.adapter.DayAdapter
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 internal class DetailedFragment : BaseFragment<DetailedFragmentBinding, DetailedViewModel>() {
@@ -12,6 +15,7 @@ internal class DetailedFragment : BaseFragment<DetailedFragmentBinding, Detailed
     override val layoutResId: Int
         get() = R.layout.detailed_fragment
     override val viewModel: DetailedViewModel by viewModel()
+    private val dayAdapter by lazy { DayAdapter() }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -21,6 +25,17 @@ internal class DetailedFragment : BaseFragment<DetailedFragmentBinding, Detailed
 
         binding.apply {
             vm = viewModel
+        }
+        setMovieRecyclerView()
+    }
+
+    private fun setMovieRecyclerView() {
+
+        with(binding) {
+            rvDetailedFragTime.apply {
+                layoutManager = GridLayoutManager(context,2)
+                adapter = dayAdapter
+            }
         }
     }
 
--- Doctor24_todoc/app/src/main/java/com/nexters/doctor24/todoc/util/BindingAdapter.kt
+++ Doctor24_todoc/app/src/main/java/com/nexters/doctor24/todoc/util/BindingAdapter.kt
@@ -4,7 +4,10 @@ import android.view.View
 import android.widget.TextView
 import androidx.annotation.DrawableRes
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.nexters.doctor24.todoc.data.detailed.response.Day
 import com.nexters.doctor24.todoc.data.detailed.response.Today
+import com.nexters.doctor24.todoc.ui.detailed.adapter.DayAdapter
 
 @BindingAdapter("android:visibility")
 fun setVisibility(view: View, isVisible: Boolean) {
@@ -15,23 +18,59 @@ fun setVisibility(view: View, isVisible: Boolean) {
 }
 
 @BindingAdapter("todayOpen")
-fun setOpenDay(view: TextView, today : Today?) {
+fun setOpenDay(view: TextView, today: Today?) {
 
-     var result =""
+    var result = ""
 
     today?.let {
 
         val start = today.startTime.split(":")
-        val end  = today.endTime.split(":")
+        val end = today.endTime.split(":")
 
-        result = "${setTime(start[0].toInt(), start[1].toInt())} ~ ${setTime(end[0].toInt(), end[1].toInt())}"
+        result = "${setTime(start[0].toInt(), start[1].toInt())} ~ ${setTime(
+            end[0].toInt(),
+            end[1].toInt()
+        )}"
     }
 
     view.text = result
 }
 
+@BindingAdapter("dayOpen")
+fun setOpenDay(view: TextView, day: Day?) {
+
+    var result = ""
+
+    day?.let {
+
+        val start = day.startTime.split(":")
+        val end = day.endTime.split(":")
+
+        result =
+            "${setHour(start[0].toInt())}:${start[1].toInt()} ~ ${setHour(end[0].toInt())}:${end[1].toInt()}"
+    }
+
+    view.text = result
+}
+
+@BindingAdapter("dayType")
+fun setDayType(view: TextView, day: String) {
+    val dayType = hashMapOf<String, String>(
+        "MONDAY" to "월요일",
+        "TUESDAY" to "화요일",
+        "WEDNESDAY" to "수요일",
+        "THURSDAY" to "목요일",
+        "FRIDAY" to "금요일",
+        "SATURDAY" to "토요일",
+        "SUNDAY" to "일요일",
+        "HOLIDAY" to "공휴일"
+    )
+    view.text = dayType[day]
+}
+
+
 @BindingAdapter("categoryText")
-fun setCategoryText(view: TextView, categories : List<String>?) {
+fun setCategoryText(view: TextView, categories: List<String>?) {
 /*
     categories?.let {
         for (i in categories){
@@ -41,12 +80,22 @@ fun setCategoryText(view: TextView, categories : List<String>?) {
     view.text = categories?.joinToString(", ")
 }
 
+@BindingAdapter("setOpenData")
+fun setOpenData(view: RecyclerView, openDatas: List<Day>?) {
+
+    (view.adapter as DayAdapter).run {
+        addItem(openDatas)
+    }
+}
+
 @BindingAdapter("android:background")
 fun setBackground(view: View, @DrawableRes resId: Int?) {
     resId?.let {
         view.setBackgroundResource(it)
     }
 }
+
+
 
 fun setAmPm(hour: Int): String {
     return if (hour >= 12)
