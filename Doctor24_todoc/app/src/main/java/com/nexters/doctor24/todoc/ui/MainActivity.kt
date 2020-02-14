18d9fb0c2a2430c253d892f41f968c850659bc1f
jiyoung <ji9310@naver.com>
2020-02-15 03:34:02 +0900

    화면 세로 고정

--- Doctor24_todoc/app/src/main/java/com/nexters/doctor24/todoc/ui/MainActivity.kt
+++ Doctor24_todoc/app/src/main/java/com/nexters/doctor24/todoc/ui/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.nexters.doctor24.todoc.ui
 
+import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.nexters.doctor24.todoc.R
@@ -7,9 +9,11 @@ import com.nexters.doctor24.todoc.ui.map.NaverMapFragment
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         supportFragmentManager.beginTransaction().replace(
             R.id.mapFragment,
             NaverMapFragment()
