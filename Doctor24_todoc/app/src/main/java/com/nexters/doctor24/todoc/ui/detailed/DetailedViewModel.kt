f70e5a9a37485839939ea3068c101fdf4b6fe690
jiyoung <ji9310@naver.com>
2020-02-17 22:02:20 +0900

    상세뷰 요일 정렬

--- Doctor24_todoc/app/src/main/java/com/nexters/doctor24/todoc/ui/detailed/DetailedViewModel.kt
+++ Doctor24_todoc/app/src/main/java/com/nexters/doctor24/todoc/ui/detailed/DetailedViewModel.kt
@@ -98,17 +98,46 @@ internal class DetailedViewModel(
             }
         }
 
-        val result = arrayListOf<DayData>()
+        val resultBundle = mutableListOf<DayData>()
+        val resultSingle = mutableListOf<DayData>()
+        val enum = WeekTypeEnum.values()
         hash.forEach { (s, arrayList) ->
-
             if (arrayList.count() > 1) {
                 val weekday = arrayList.joinToString(separator = ",")
-                result.add(DayData(s, weekday))
+                if(weekday.contains("월") || weekday.contains("화")) {
+                    resultBundle.add(0, DayData(s, weekday))
+                } else {
+                    resultBundle.add(DayData(s, weekday))
+                }
             } else {
-                result.add(DayData(s, "${arrayList[0]}요일"))
+                resultSingle.add(DayData(s, "${arrayList[0]}요일"))
             }
         }
-        return result
+        resultSingle.sortBy { day->
+            enum.find { it.title == day.weekday }
+        }
+
+        Timber.d("resultBundle : $resultBundle")
+        Timber.d("resultSingle : $resultSingle")
+
+        resultBundle.addAll(resultSingle)
+        val array = arrayListOf<DayData>()
+        resultBundle.forEach {
+            array.add(it)
+        }
+        return array
+    }
+
+    enum class WeekTypeEnum(val index: Int, val dayType: String, val title: String) {
+        EMPTY(-1, "", ""),
+        MONDAY(0, "MONDAY", "월요일"),
+        TUESDAY(1, "TUESDAY", "화요일"),
+        WEDNESDAY(2, "WEDNESDAY", "수요일"),
+        THURSDAY(3, "THURSDAY", "목요일"),
+        FRIDAY(4, "FRIDAY", "금요일"),
+        SATURDAY(5, "SATURDAY", "토요일"),
+        SUNDAY(6, "SUNDAY", "일요일"),
+        HOLIDAY(7, "HOLIDAY", "공휴일");
     }
 
     internal data class SelectedMarkerUIData(
